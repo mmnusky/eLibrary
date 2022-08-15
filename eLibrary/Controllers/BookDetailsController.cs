bd9ccff19b6868b47a292cd68cea866e9b0d9d76
Mohamed Nusky <nusky.uom@gmail.com>
2022-08-15 13:14:14 +0530

    Code fixes

--- eLibrary/Controllers/BookDetailsController.cs
+++ eLibrary/Controllers/BookDetailsController.cs
@@ -50,39 +50,7 @@ namespace eLibrary.Controllers
         }
         #endregion
 
-        #region image upload
-
-        [HttpPost, DisableRequestSizeLimit]
-        public async Task<IActionResult> Upload()
-        {
-            try
-            {
-                var formCollection = await Request.ReadFormAsync();
-                var file = formCollection.Files.First();
-                var folderName = Path.Combine("Resources", "Images");
-                var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
-                if (file.Length > 0)
-                {
-                    var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
-                    var fullPath = Path.Combine(pathToSave, fileName);
-                    var dbPath = Path.Combine(folderName, fileName);
-                    using (var stream = new FileStream(fullPath, FileMode.Create))
-                    {
-                        file.CopyTo(stream);
-                    }
-                    return Ok(new { dbPath });
-                }
-                else
-                {
-                    return BadRequest();
-                }
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, $"Internal server error: {ex}");
-            }
-        }
-        #endregion
+      
 
         #region GetBookDetailsByID
 
@@ -188,6 +156,40 @@ namespace eLibrary.Controllers
                 error.StatusCode = 400;
                 error.Message = Constant.DelateBookError;
                 return BadRequest(error);
+            }
+        }
+        #endregion
+
+        #region image upload
+
+        [HttpPost, DisableRequestSizeLimit]
+        public async Task<IActionResult> Upload()
+        {
+            try
+            {
+                var formCollection = await Request.ReadFormAsync();
+                var file = formCollection.Files.First();
+                var folderName = Path.Combine("Resources", "Images");
+                var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+                if (file.Length > 0)
+                {
+                    var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                    var fullPath = Path.Combine(pathToSave, fileName);
+                    var dbPath = Path.Combine(folderName, fileName);
+                    using (var stream = new FileStream(fullPath, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+                    return Ok(new { dbPath });
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex}");
             }
         }
         #endregion
